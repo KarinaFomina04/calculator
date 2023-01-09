06b20eff24313120ba56ac52611e32c99d630a2c
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-01-09 14:52:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const screenDisplay = document.querySelector('.screen');
+const buttons = document.querySelectorAll('button');
+
+let calculation = [];
+let accumulativeCalculation;
+
+function calculate(button){
+    const value = button.textContent;
+    if (value === "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    } else if (value === "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value);
+        accumulativeCalculation = calculation.join('');
+        screenDisplay.textContent = accumulativeCalculation;
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)));
